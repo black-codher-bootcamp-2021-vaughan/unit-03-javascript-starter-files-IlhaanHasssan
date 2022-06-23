7e18d669aa98f8eb79cc298eea280aec79b792d5
Ilhaan <IlhaanHassanA@outlook.com>
2022-06-23 20:39:46 +0100

    function

--- session-3/index.js
+++ session-3/index.js
@@ -2,12 +2,38 @@
 // You can see where this file is connected to your HTML in the index.html on line 14
 
 
-const body = document.body.parentNode
-var description = document.getElementById('description')
-var about = document.getElementsByClassName('.about')
-var title = document.getElementById('title')
+// const body = document.body.parentNode
+// var description = document.getElementById('description')
+// var about = document.getElementsByClassName('.about')
+// var title = document.getElementById('title')
 
-console.log(body)
-console.log(description)
-console.log(about)
-console.log(title)
+// console.log(body)
+// console.log(description)
+// console.log(about)
+// console.log(title) 
+
+// console.log("hello world");
+// const a = 1
+// const b = 4
+// const c = a + b 
+// console.log(c);
+function sayHey() {
+console.log("Hey!");
+}
+// sayHey();
+// function conversation() {
+//     sayHey();
+//     console.log("Hello");
+//     console.log("Goodbye");
+//     }
+
+//     conversation();  
+
+
+    function futureAge(name , age) {
+        let text = "Hey, Im " + name + " and I'll be " + (age + 5) + "in 5 years time!";
+         return text;
+    }
+
+    console.log(futureAge("Jason", 6));
+   
