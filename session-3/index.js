e9bc5354008507519e29459f7db8d8747fc4a4a2
Ilhaan <IlhaanHassanA@outlook.com>
2022-06-23 18:52:25 +0100

    first

--- session-3/index.js
+++ session-3/index.js
@@ -1,2 +1,13 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+
+
+const body = document.body.parentNode
+var description = document.getElementById('description')
+var about = document.getElementsByClassName('.about')
+var title = document.getElementById('title')
+
+console.log(body)
+console.log(description)
+console.log(about)
+console.log(title)
